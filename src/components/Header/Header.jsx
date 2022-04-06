e21e35bf82ab1f8cfd624a0fbcd30e6d6ded2139
William58141 <william.fosmark@outlook.com>
2022-04-06 16:38:55 +0200

    Update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+
+const Header = () => {
+    return(
+        <h1>Header</h1>
+    );
+}
+
+export default Header;
