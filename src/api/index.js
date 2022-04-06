3de158a63840fccdf26859f61975bdc2d7363275
William58141 <william.fosmark@outlook.com>
2022-04-07 01:11:27 +0200

    Changes

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(URL, {
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+              'X-RapidAPI-Key': 'c38b51f087msha0b3bb26040ba00p1319f8jsn0eec7520d5f2'
+            }
+          });
+
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
 
-const Map = () => {
+import useStyles from './styles';
+
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+
     return(
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyAi0odWrmUgTXt3yvXn8B66FPDosifaL-s' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                }}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
