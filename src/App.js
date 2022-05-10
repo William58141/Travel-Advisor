d740fdfa5a1c6e8a70ad4672ec6f0a2a03935bad
William58141 <willam.fosmark@outlook.com>
2022-05-10 20:17:22 +0200

    Updated list

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ import Map from './components/Map/Map';
 
 const App = () => {
     const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
 
     const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState(null);
+    const [bounds, setBounds] = useState({});
+
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
@@ -20,11 +23,12 @@ const App = () => {
     }, []);
 
     useEffect(() => {
+        setIsLoading(true);
+
         getPlacesData(bounds.sw, bounds.ne)
             .then((data) => {
-                console.log(data);
-
                 setPlaces(data);
+                setIsLoading(false);
             })
     }, [coordinates, bounds]);
 
@@ -34,13 +38,19 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List places={places} />
+                    <List
+                        places={places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                     />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map 
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        places={places}
+                        setChildClicked={setChildClicked}
                     />
                 </Grid>
             </Grid>
